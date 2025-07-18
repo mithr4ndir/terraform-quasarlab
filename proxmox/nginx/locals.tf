e0aed1d4414612b43841e2cd3d8684f7c0cabdaf
Chris Lad <cwladino@outlook.com>
2025-07-17 20:51:10 -0700

    adding nginx and correcting fleetdm

---
+++ proxmox/nginx/locals.tf
@@ -0,0 +1,35 @@
+locals {
+  sshkeys = file("~/.ssh/id_rsa.pub")
+  vms = {
+    nginx1 = {
+      template        = "ubuntu-24-04-cloud-init-template"
+      username        = var.vm_defaults.username
+      password        = var.vm_defaults.password
+      memory          = 4096
+      cores           = 2
+      sockets         = 1
+      storage_pool    = var.vm_defaults.storage_pool
+      storage_size    = var.vm_defaults.storage_size
+      network_bridge  = var.vm_defaults.network_bridge
+      skip_ipv6       = true
+      onboot          = true
+      full_clone      = true
+      hotplug         = "network,disk,usb,memory,cpu"
+    }
+    nginx2 = {
+      template        = "ubuntu-24-04-cloud-init-template"
+      username        = var.vm_defaults.username
+      password        = var.vm_defaults.password
+      memory          = 4096
+      cores           = 2
+      sockets         = 1
+      storage_pool    = var.vm_defaults.storage_pool
+      storage_size    = var.vm_defaults.storage_size
+      network_bridge  = var.vm_defaults.network_bridge
+      skip_ipv6       = true
+      onboot          = true
+      full_clone      = true
+      hotplug         = "network,disk,usb,memory,cpu"
+    }
+  }
+}
