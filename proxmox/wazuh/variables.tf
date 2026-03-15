d16ff521f98c3918ac11cde50bd40e58c1d7696a
Chris Ladino <cwladino@outlook.com>
2026-03-15 01:24:49 +0000

    Add Wazuh VM on pve at 192.168.1.171
    16GB RAM, 4 cores, static IP, Ubuntu 24.04 cloud-init template.
    All-in-one Wazuh deployment (manager + indexer + dashboard).

---
+++ proxmox/wazuh/variables.tf
@@ -0,0 +1,36 @@
+variable "pm_user" {
+  type = string
+}
+
+variable "pm_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "pm_api_url" {
+  type = string
+}
+
+variable "pm_node" {
+  type = string
+}
+
+variable "vm_defaults" {
+  description = "Default values for VM deployment"
+  sensitive = true
+  type = object({
+    username        = string
+    password        = string
+    storage_pool    = string
+    storage_size    = string
+    network_bridge  = string
+  })
+
+  default = {
+    username        = "ubuntu"
+    password        = "changeme"
+    storage_pool    = "truenas-iscsi"
+    storage_size    = "54784M"
+    network_bridge  = "vmbr0"
+  }
+}
